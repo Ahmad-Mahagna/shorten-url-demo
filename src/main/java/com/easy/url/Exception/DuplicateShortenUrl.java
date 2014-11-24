2ef94360d566801455578c8333357a10e0ec0625
Ahmad-Mahagna <ahmad_khalid2002@hotmail.com>
2014-11-24 21:01:41 +0200

    Update DuplicateShortenUrl.java

--- src/main/java/com/easy/url/Exception/DuplicateShortenUrl.java
+++ src/main/java/com/easy/url/Exception/DuplicateShortenUrl.java
@@ -1,11 +1,18 @@
 package com.easy.url.Exception;
 
 /**
+ * Internal Exception DuplicateShortenUrl Exception  
+ * USE CASE : Two entities at least in db have same id , key=[EasyURL] exist at least twice .
+ * 
  * Created by amahagna on 11/24/14.
+ * 
  */
 public class DuplicateShortenUrl extends Exception {
 
-
+  /**
+   *          Constructor 
+   * @see Exception 
+   */
     public DuplicateShortenUrl(String message) {
         super(message);
     }
