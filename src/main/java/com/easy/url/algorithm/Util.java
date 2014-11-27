896d140a4c78e447de85033a19ceb94228363897
Ahmad-Mahagna <ahmad_khalid2002@hotmail.com>
2014-11-27 10:24:57 +0200

    prototype random algorithm

--- src/main/java/com/easy/url/algorithm/Util.java
+++ src/main/java/com/easy/url/algorithm/Util.java
@@ -3,7 +3,7 @@ package com.easy.url.algorithm;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * Created by amahagna on 11/24/14.
+ * Util class represent static methods used for algorithm 
  *
  * @author Ahmad Mahagna
  */
@@ -12,8 +12,12 @@ public class Util {
     private static final AtomicLong INC = new AtomicLong();
     private static final long xFF = 0xFF;
 
-
+    /**
+     *  algorithm (prototype)  get current time in long + counter.
+     *  @return long represent random long.
+     */
     public static long getRandomLong() {
+        //TODO improve the random method currently its prototype    CRC32 / hashCode / counter / instanceId / UUID / USID ...
         return (System.currentTimeMillis() + (INC.getAndIncrement() & xFF));
 
     }
